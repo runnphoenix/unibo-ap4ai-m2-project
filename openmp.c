16e741c091c5ba4c3f967c966b1de09d54c8923c
Zhang Hanying <runnphoenix@gmail.com>
2021-08-01 15:07:20 +0800

    tidy code in opemp

--- openmp.c
+++ openmp.c
@@ -8,7 +8,8 @@
 #define R 3
 int n_threads;
 
-void single_layer_calc(float *x, float (*W)[R], float b, float *y, int N)
+/* The calculation of y values for one layer */
+void one_layer_calc(float *x, float (*W)[R], float b, float *y, int N)
 {
 	int i,j;
 	#pragma omp parallel for private(j) num_threads(n_threads)
@@ -21,6 +22,7 @@ void single_layer_calc(float *x, float (*W)[R], float b, float *y, int N)
 	}
 }
 
+/* Initialize the W and b parameters for one layer */
 void init_layer_parameters(float (*W)[R], float w_v, float *b, float b_v, int layer_len)
 {
 	#pragma omp parallel for collapse(2) num_threads(n_threads)
@@ -35,9 +37,10 @@ void init_layer_parameters(float (*W)[R], float w_v, float *b, float b_v, int la
 	*b = b_v;
 }
 
+/* Read in the network parameters (N, K and # threads) from command-line input.
+   the library used here is getopt (GNU) from unistd.h. */
 void parse_command_line_parameters(int argc, char *argv[], int *n_threads, int *N, int *K)
 {
-	// the library used here is getopt (GNU) from unistd.h
 	int c;
 	while ((c = getopt (argc, argv, "t:n:k:")) != -1)
 	{
@@ -75,17 +78,14 @@ int main(int argc, char *argv[])
 		return EXIT_FAILURE;
 	}
 
-	// initialize the values of the input to 1
-	float x[N];
-	for (int i=0; i < N; i++)
+	float x[N]; // the first layer
+	float latest_layer[N]; // array for storing the latest layer got calculated
+	
+	for (int i=0; i < N; i++) // initialize x
 	{
 		x[i] = -1.0;
 	}
-
-	// Create an array for storing the latest layer got calculated
-	float latest_layer[N];
-	// copy the values from x, making x the values of layer 0
-	memcpy(latest_layer, x, N * sizeof(float));
+	memcpy(latest_layer, x, N * sizeof(float)); // the lastest layer is the first layer at the beginning
 
 	// start recording time
 	float t_start = omp_get_wtime();
@@ -93,8 +93,7 @@ int main(int argc, char *argv[])
 	// Loop over K layers
 	for(int t=1; t<K; t++)
 	{
-		// calculate length of this layer
-		int layer_len = N - t * (R-1);
+		int layer_len = N - t * (R-1);        // calculate length of this layer
 		int in_layer_len = layer_len + R - 1; // the length of the input layer
 
 		// create w, b and y
@@ -102,18 +101,15 @@ int main(int argc, char *argv[])
 		float b;
 		float y[layer_len]; // layer result
 		
-		// random Initialization to values in range [-1,1]
-		//float W_v = ((rand() % 2000) - 1000) / 1000.0; // 3 effective digits after 0.
+		//float W_v = ((rand() % 2000) - 1000) / 1000.0; // random Initialization to values in range [-1,1]
 		//float b_v = rand() % 3 - 1; // random value from {-1, 0, 1}
 		float b_v = 1.0;
 		float W_v = 1.0 / 3;
 		init_layer_parameters(W, W_v, &b, b_v, layer_len);
 
-		// calculation on this layer, update y
-		single_layer_calc(latest_layer, W, b, y, in_layer_len);
+		one_layer_calc(latest_layer, W, b, y, in_layer_len); // calculation on this layer, update y
 
-		// update the values of latest_layer
-		memcpy(latest_layer, y, layer_len * sizeof(float));
+		memcpy(latest_layer, y, layer_len * sizeof(float));  // update the values of latest_layer
 	}
 
 	// stop recording time
